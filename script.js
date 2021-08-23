83692666da01703607abd2df98c47ccd6fc2670c
Vyacheslav <slavebery@gmail.com>
2021-08-23 23:39:37 +0500

    init

---
+++ script.js
@@ -0,0 +1,32 @@
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b != '') {
+          personalMovieDB.movies[a] = b;
+          console.log('done')
+        }else {
+            console.log('error');
+            i--;
+        }
+} ;
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов.')
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы класический зритель.')
+} else if(personalMovieDB.count > 30) {
+    alert(' Вы киноман.')
+} else {
+    alert('Произошла ошибка.')
+};
+console.log(personalMovieDB);
